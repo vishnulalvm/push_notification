544666336f3696ee88544644d851eed0b52d73ab
vishnulalvm <vishnulalvm007@gmail.com>
2024-08-28 10:52:03 +0530

    fix bug

--- lib/notification_sevices.dart
+++ lib/notification_sevices.dart
@@ -6,26 +6,35 @@ class NotificationSevices {
 
   Future<void> initNotification() async {
     AndroidInitializationSettings initializationSettingsAndroid =
-        const AndroidInitializationSettings('flutter_logo');
+        const AndroidInitializationSettings('ic_launcher');
     var initializationSettingsIOS = DarwinInitializationSettings(
         requestAlertPermission: true,
         requestBadgePermission: true,
         requestSoundPermission: true,
         onDidReceiveLocalNotification:
             (int id, String? title, String? body, String? payload) async {});
-    var initializationSetting = InitializationSettings(android: initializationSettingsAndroid,iOS: initializationSettingsIOS);
-  await notificationsPlugin.initialize(initializationSetting,onDidReceiveNotificationResponse: (NotificationResponse notificationResponse)async{});
+    var initializationSetting = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    await notificationsPlugin.initialize(initializationSetting,
+        onDidReceiveNotificationResponse:
+            (NotificationResponse notificationResponse) async {});
   }
 
-  notificationDetails(){
+  notificationDetails() {
     return const NotificationDetails(
-      android: AndroidNotificationDetails("channelId", "channelName",importance: Importance.max),iOS: DarwinNotificationDetails()
-    );
-
+        android: AndroidNotificationDetails("channelId", "channelName",
+            importance: Importance.max,
+             icon: 'ic_launcher',),
+        iOS: DarwinNotificationDetails());
   }
 
-  Future showNotification({int id =0,String? title,String?body,String? payload})async{
-return notificationsPlugin.show(id, title, body, await notificationDetails());
-
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payload}) async {
+    try {
+      await notificationsPlugin.show(
+          id, title, body, await notificationDetails());
+    } catch (e) {
+      print("Error showing notification: $e");
+    }
   }
 }
